5a7b3348f78adca567ce7cdf601fb07a76fecd74
nsk0826 <nsk.20160401@gmail.com>
2022-04-24 23:58:17 +0900

    import する記述に変更したがエラー。。。

--- src/push.js
+++ src/push.js
@@ -1,6 +1,2 @@
-<>
-  <script src="https://cdnjs.cloudflare.com/ajax/libs/push.js/0.0.11/push.min.js"></script>
-  <script>
-    push.create('更新情報');
-  </script>
-</>
+import { Push } from "https://cdnjs.cloudflare.com/ajax/libs/push.js/0.0.11/push.min.js";
+Push.create('更新情報');
