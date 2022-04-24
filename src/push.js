fb8f7f4c580ad7598c177de8ebce137465a2fcf6
nsk0826 <nsk.20160401@gmail.com>
2022-04-24 23:45:31 +0900

    修正

--- src/push.js
+++ src/push.js
@@ -1,5 +1,6 @@
-<script src="https://cdnjs.cloudflare.com/ajax/libs/push.js/0.0.11/push.min.js"></script>
-function push() {
-  push.create('更新情報');
-}
-push();
+<>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/push.js/0.0.11/push.min.js"></script>
+  <script>
+    push.create('更新情報');
+  </script>
+</>
